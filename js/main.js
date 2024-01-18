cb8bc557fdd7c2016ee8a9957cbcd16f866e6bf2
Max Aberukov <AberukovWeb@gmail.com>
2024-01-18 18:23:39 +0300

    готовый верстка с адаптацией

--- js/main.js
+++ js/main.js
@@ -2,16 +2,17 @@ const infoBtns = document.querySelectorAll('.info-dot');
 const infoHints = document.querySelectorAll('.info-hint');
 
 // Клик по кнопкам с подсказками
+
 for (let btn of infoBtns) {
 	btn.addEventListener('click', function (e) {
 		e.stopPropagation();
 
 		// Hide all hint
-		for (let hint of infoHints) {
+		/* for (let hint of infoHints) {
 			if (this.parentNode.querySelector('.info-hint') !== hint) {
 				hint.classList.add('none');
 			}
-		}
+		} */
 
 		// Show current hint
 		this.parentNode.querySelector('.info-hint').classList.toggle('none');
@@ -19,6 +20,7 @@ for (let btn of infoBtns) {
 }
 
 // Закрываем подсказки при клике по всему документу
+
 document.addEventListener('click', function () {
 	for (let hint of infoHints) {
 		hint.classList.add('none');
@@ -26,6 +28,93 @@ document.addEventListener('click', function () {
 });
 
 // Запрещаем всплытие события клика при клике на подсказки
+
 for (let hint of infoHints) {
 	hint.addEventListener('click', (e) => e.stopPropagation());
+}
+
+//Swiper slider
+
+const swiper = new Swiper('.swiper', {
+	loop: true,
+	freeMode:true,
+
+	slidesPerView: 1,
+	spaceBetween: 42,
+
+	breakpoints: {
+        600: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        920: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1230: {
+          slidesPerView: 4,
+          spaceBetween: 42,
+        },
+      },
+
+  
+	// Navigation arrows
+	
+	navigation: {
+	  nextEl: '#sliderNext',
+	  prevEl: '#sliderPrev',
+	},
+  });
+
+	//Tabs
+
+const tabsBtns = document.querySelectorAll('[data-tab]');
+const tabsProducts = document.querySelectorAll('[data-tab-value]');
+
+
+for (let btn of tabsBtns) {
+
+
+	btn.addEventListener('click', function () {
+
+		// Убираем активный класс у всех кнопок
+		for (let btn of tabsBtns) {
+			btn.classList.remove('tab-controls__btn--active');
+		}
+
+		// Добавляем активный класс к текущей кнопке
+		this.classList.add('tab-controls__btn--active');
+
+		// Отобразить нужные товары и скрыть не нужные
+		for (let product of tabsProducts) {
+
+			// Проверка на отображение всех товаров
+			if (this.dataset.tab === 'all') {
+				product.classList.remove('none');
+			} else {
+				if (product.dataset.tabValue === this.dataset.tab) {
+					product.classList.remove('none');
+				} else {
+					product.classList.add('none')
+				}
+			}
+		}
+
+		// Swiper update
+		swiper.update()
+	})
+}
+
+// Mobile Nav
+
+const mobileNavOpenBtn = document.querySelector('#open-mobile-nav-btn');
+const mobileNavCloseBtn = document.querySelector('#close-mobile-nav-btn');
+const mobileNav = document.querySelector('#mobile-nav');
+
+mobileNavOpenBtn.onclick = function () {
+	mobileNav.classList.add('mobile-nav-wrapper--open');
+}
+
+mobileNavCloseBtn.onclick = function () {
+	mobileNav.classList.remove('mobile-nav-wrapper--open');
 }
